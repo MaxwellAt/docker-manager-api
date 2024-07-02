d9913689bd245b2009b1af666cfa4ba368e51727
MarcosGTS <marcosgtsodre@gmail.com>
2024-07-02 15:32:35 -0300

    Add filename generation

--- src/scriptManager.js
+++ src/scriptManager.js
@@ -8,7 +8,16 @@ const mustache = require("mustache");
 const TEST_FILE_FOLDER = path.join(__dirname, "..", "test_files");
 const types = ["smoke", "stress", "breakpoint", "spike", "load", "soak"];
 
-async function generateScript(url, type) {
+function getFilename(application) {
+  let { backend, database } = application;
+  let filename = `${application.conf}`;
+  filename += `_${backend.cpu}_${backend.ram}`;
+  filename += `_${database.cpu}_${database.ram}`;
+
+  return path.join(os.tmpdir(), `${filename}.js`);
+}
+
+async function generateScript(url, type, application) {
   // TO-DO: Need to check type is a valid option
   if (!types.includes(type)) return { success: false };
 
@@ -20,7 +29,8 @@ async function generateScript(url, type) {
   const fileContent = mustache.render(template, { url });
 
   try {
-    const filename = path.join(os.tmpdir(), `${type}_test.js`);
+    const filename = getFilename(application);
+    //const filename = path.join(os.tmpdir(), `${type}_test.js`);
     await writeFile(filename, fileContent, "utf-8");
     return { success: true, value: filename };
   } catch (e) {
